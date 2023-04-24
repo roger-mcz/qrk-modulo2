f3327459120f8064ca3b7d444eb3556afa36c667
Rogerio Oliveira <roger.corpore@gmail.com>
2023-04-24 12:37:44 -0300

    reserva ok

--- agencia-app/src/main/java/reserva/Reserva.java
+++ agencia-app/src/main/java/reserva/Reserva.java
@@ -1,5 +1,6 @@
 package reserva;
 
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 
@@ -7,8 +8,10 @@ import org.cliente.Cliente;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
+@Entity
 public class Reserva extends PanacheEntity {
     
     @ManyToOne(fetch = FetchType.LAZY)
     public Cliente cliente;
+    
 }
